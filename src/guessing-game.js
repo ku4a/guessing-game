3f7b6f3050b4dd2ef90daebf481d7f198a96348f
ku4a <7916884@gmail.com>
2023-02-20 10:45:50 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.current = Math.round((this.max - this.min) / 2 + this.min);
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
